5a6dfa758571a3f62a4a3dc95ddc4c2d740ee840
ITAlonsoRabanal <nachoalonso273@gmail.com>
2026-05-19 23:58:11 -0300

    Updated for deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,16 +3,17 @@ import { DataSourceOptions } from 'typeorm';
 
 export default registerAs('typeorm', () => ({
   type: 'postgres',
-  database: process.env.DB_NAME || 'mydatabase',
-  host: process.env.DB_HOST || 'localhost',
+  database: process.env.PROD_DB_NAME || process.env.DB_NAME || 'mydatabase',
+  url: process.env.PROD_DATABASE_URL,
+  host: process.env.PROD_DB_HOST || process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
+  username: process.env.PROD_DB_USERNAME || process.env.DB_USERNAME || 'postgres',
+  password: process.env.PROD_DB_PASSWORD || process.env.DB_PASSWORD || 'password',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
   dropSchema: false,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   ssl: process.env.DB_SSL === 'true',
 }) as DataSourceOptions);
